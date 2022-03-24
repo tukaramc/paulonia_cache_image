06358a01f8cb695fdd31234e0275f76bb3aae71b
chetan-cueclad <39045302+chetan-cueclad@users.noreply.github.com>
2022-03-24 13:41:48 +0530

    Added HiveFields.
    Added HiveFields.

--- lib/hive_cache_image.dart
+++ lib/hive_cache_image.dart
@@ -8,13 +8,21 @@ part 'hive_cache_image.g.dart';
 @HiveType(typeId: Constants.HIVE_ADAPTER_ID)
 class HiveCacheImage {
   @HiveField(0)
-  String url;
+  String remotePath;
 
   @HiveField(1)
-  Uint8List binaryImage;
+  Uint8List? binaryImage;
+
+  @HiveField(2)
+  int? version;
+
+  @HiveField(3)
+  String? localPath;
 
   HiveCacheImage({
-    required this.url,
-    required this.binaryImage,
+    required this.remotePath,
+    required this.version,
+    this.binaryImage,
+    this.localPath,
   });
 }
