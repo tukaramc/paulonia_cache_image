544d23249010af0f3f1a7b49263ff7e6bab1e18b
chetan-cueclad <39045302+chetan-cueclad@users.noreply.github.com>
2022-03-24 13:44:28 +0530

    Update cache_refresh_strategy.dart

--- lib/cache_refresh_strategy.dart
+++ lib/cache_refresh_strategy.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: constant_identifier_names
 enum CacheRefreshStrategy {
-  // BY_METADATA_DATE uses the Storage Object updated timestamp as a version
+  // PROGRESSIVE and NATIVE use the Storage Object updated timestamp as a version
   // number and checks for updates every time.
   NATIVE,
+  
+  PROGRESSIVE,
+  
   // NEVER will never check for any updates. It will still reload a previously-
   // cached version that has been cleaned up by the OS.
-  PROGRESSIVE,
   NONE,
 }
